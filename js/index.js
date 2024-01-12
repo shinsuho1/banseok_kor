757702280cf093623d0d495ab731b07c0c7b4f35
shinsuho1 <suhoshin1816@gmail.com>
2024-01-12 17:07:20 +0900

    j

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+    const news_slide = new Swiper('.news_slide', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+    });    
+})
+
+
+
+
+
+// $("#header #gnb").hover(function(){
+//   $("#header").toggleClass("on");
+// })
+
+const pop_gnb_li = document.querySelectorAll(".pop_gnb>ul>li");
+
+$("#menuicon").on("click",function(){
+  $(".util").toggleClass("on");
+  $(".pop_gnb").toggleClass("active");
+})
+
+
+pop_gnb_li.forEach((el,index)=>{
+  el.addEventListener("click",()=>{
+    pop_gnb_li.forEach((el)=>{
+      el.classList.remove("on");
+    });
+    el.classList.add("on");
+  });
+});
